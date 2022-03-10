3dc7084075be6a212d8fdf058f27568422c88c30
clcoulter46 <clcoulter46@gmail.com>
2022-03-10 15:15:26 -0500

    finished setup for real

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const morgan = require('morgan');
+const layout = require('./views/layout');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
+
+// app.use('',(req,res)=>{
+//   res.sendFile(__dirname + '/public/style.css')
+// }) //this gave us the text of the style sheet.
+
+app.use('',(req,res)=>{
+    res.send(layout());
+})
 
 app.get('/',(req,res)=>{
   res.send("hello world");
